bfc82b9bd441bfade801ff3be974b4d7c18aca35
Sawan Y <187062279+Ysawan2@users.noreply.github.com>
2024-12-27 05:30:01 -0800

    fixed issue with installer

--- deploy-workbench.sh
+++ deploy-workbench.sh
@@ -1,26 +1,39 @@
-!#/bin/bash
+#!/bin/bash
 
 K3D_INSTALL_CMD='curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash'
 KUBECTL_INSTALL_CMD='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"'
 
 # color codes
+# RED="\e[31m"
+# GREEN="\e[32m"
+# RESET="\e[0m"
+
 RED="\e[31m"
-GREEN="\e[1;32m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
 RESET="\e[0m"
 
 install_dependencies(){
     # install kubectl
-    output=$(KUBECTL_INSTALL_CMD)
-    if [ $? -ne 0 ]; then
+    #KUBECTL_INSTALL_CMD="curl -LO 'https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl'"
+    #KUBECTL_INSTALL_CMD='ls -l'
+    eval $KUBECTL_INSTALL_CMD
+    if [ ! -f "kubectl" ]; then
         echo -e "${RED} Failed to install kubectl, cannot proceed further..${RESET}"
         exit 1
     fi
-    echo -e "${GREEN}Dependencies installed successfully...${RESET}"
+    chmod +x kubectl
+    sudo mv kubectl /usr/local/bin/kubectl
+    kubectl version --client
+    echo -e "${GREEN}Kubectl installed successfully...${RESET}"
 }
 
 install(){
     # install k3d
-    output=$(K3D_INSTALL_CMD)
+    eval $K3D_INSTALL_CMD
+    #sudo mv k3d /usr/local/bin/k3d
+    eval "k3d --version"
     if [ $? -ne 0 ]; then
         echo -e "${RED} Failed to install K3d...quitting${RESET}"
         exit 1
