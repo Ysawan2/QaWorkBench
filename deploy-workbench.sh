3506724ef05707657b971a68623bad1bca7b4b29
Ysawan2 <187062279+Ysawan2@users.noreply.github.com>
2024-12-28 21:52:53 -0800

    Feat configurator (#4)
    * fixed issue with installer
    
    * added cli installation
    
    * updated readme

--- deploy-workbench.sh
+++ deploy-workbench.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-K3D_INSTALL_CMD='curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash'
-KUBECTL_INSTALL_CMD='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"'
+# Linux installers
+K3D_INSTALL_CMD_LINUX='curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash'
+KUBECTL_INSTALL_CMD_LINUX='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"'
+AWS_INSTALL_CMD_LINUX='curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"; sudo installer -pkg AWSCLIV2.pkg -target /'
+AZURE_INSTALL_CMD_LINUX='brew install azure-cli'
+
+# MAC installers
+K3D_INSTALL_CMD_MAC='curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash'
+KUBECTL_INSTALL_CMD_MAC='curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"'
+AWS_INSTALL_CMD_MAC='curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"; sudo installer -pkg AWSCLIV2.pkg -target /'
+AZURE_INSTALL_CMD_MAC='brew install azure-cli'
+GCLOUD_INSTALL_CMD_MAC='./google-cloud-sdk/install.sh'
 
 # color codes
 # RED="\e[31m"
@@ -14,14 +24,22 @@ YELLOW="\e[33m"
 BLUE="\e[34m"
 RESET="\e[0m"
 
+OS_TYPE=$(uname)
+if [ "$OS_TYPE" == "Darwin" ]; then
+    OS_TYPE="MAC"
+elif [ "$OS_TYPE" == "Linux" ]; then
+    OS_TYPE="LINUX"
+fi
+
 install_dependencies(){
     # install kubectl
-    #KUBECTL_INSTALL_CMD="curl -LO 'https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl'"
-    #KUBECTL_INSTALL_CMD='ls -l'
-    eval $KUBECTL_INSTALL_CMD
-    if [ ! -f "kubectl" ]; then
+    INSTALL_CMD="$KUBECTL_INSTALL_CMD_$OS_TYPE"
+    eval ${!INSTALL_CMD}
+
+    if [[ $? -ne 0 && -f "kubectl" ]]; then
+
         echo -e "${RED} Failed to install kubectl, cannot proceed further..${RESET}"
-        exit 1
+        #exit 1
     fi
     chmod +x kubectl
     sudo mv kubectl /usr/local/bin/kubectl
@@ -29,17 +47,50 @@ install_dependencies(){
     echo -e "${GREEN}Kubectl installed successfully...${RESET}"
 }
 
-install(){
+deploy_cluster(){
     # install k3d
-    eval $K3D_INSTALL_CMD
+
+    INSTALL_CMD="$K3D_INSTALL_CMD_$OS_TYPE"
+    eval ${!INSTALL_CMD}
+
     #sudo mv k3d /usr/local/bin/k3d
     eval "k3d --version"
     if [ $? -ne 0 ]; then
         echo -e "${RED} Failed to install K3d...quitting${RESET}"
         exit 1
     fi
-    echo -e "${GREEN}K3d installation complete...${RESET}"
+    echo -e "$GREEN K3d installation complete...$RESET"
+}
+
+install_clis(){
+    # aws cli
+    INSTALL_CMD="$AWS_INSTALL_CMD_$OS_TYPE"
+    eval ${!INSTALL_CMD}
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED} Failed to install aws cli..exiting ${RESET}"
+    fi
+
+    INSTALL_CMD="$AZURE_INSTALL_CMD_$OS_TYPE"
+    eval ${!INSTALL_CMD}
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED} Failed to install azure cli..exting ${RESET}"
+    fi
+
+    INSTALL_CMD="$GCLOUD_INSTALL_CMD_$OS_TYPE"
+    eval ${!INSTALL_CMD}
+
+    echo "================================================="
+    output=$(aws --version)
+    echo -e "${YELLOW} AWS CLI Version: ${output} ${RESET}"
+    output=$(az version)
+    echo -e "${YELLOW} AZURE CLI Version: ${output} ${RESET}"
+    output=$(gcloud version)
+    echo -e "${YELLOW} GCLOUD CLI Version: ${output} ${RESET}"
+    echo "================================================="
 }
 
 install_dependencies
-install
+deploy_cluster
+install_clis
